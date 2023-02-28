ee0d49402b5bfa8eb41b76890a445471036c36fb
arifjokam <arif.hidayatulloh313@gmail.com>
2023-02-28 17:12:50 +0700

    Finish user, camp, camp benefit, checkout

--- database/migrations/2023_02_28_095518_create_checkouts_table.php
+++ database/migrations/2023_02_28_095518_create_checkouts_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Brick\Math\BigInteger;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,12 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('camp_benefits', function (Blueprint $table) {
+        Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
-            // $table->bigInteger('camp_id');
+            $table->foreignId('user_id')->constrained();
             $table->foreignId('camp_id')->constrained();
-            $table->string('name');
+            $table->string('card_number', 20);
+            $table->date('expired');
+            $table->string('cvc', 3);
+            $table->boolean('is_paid')->default(false);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -26,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('camp_benefits');
+        Schema::dropIfExists('checkouts');
     }
 };
